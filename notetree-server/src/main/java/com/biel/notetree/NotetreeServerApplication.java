4318f3d102addc0d01e0ba8a4f2c4fe2e8537884
Biel Simon Rojas <bielsimon2@gmail.com>
2016-11-13 17:01:03 +0100

    Initial commit, adding server project

---
+++ notetree-server/src/main/java/com/biel/notetree/NotetreeServerApplication.java
@@ -0,0 +1,12 @@
+package com.biel.notetree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotetreeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotetreeServerApplication.class, args);
+	}
+}
