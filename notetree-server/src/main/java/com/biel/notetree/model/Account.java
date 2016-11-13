ad857cffda3b6e52a0dbf3150ea1584b263993fb
Biel Simon Rojas <bielsimon2@gmail.com>
2016-11-13 17:15:00 +0100

    Created model

---
+++ notetree-server/src/main/java/com/biel/notetree/model/Account.java
@@ -0,0 +1,81 @@
+package com.biel.notetree.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Biel on 11/11/2016.
+ */
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String username;
+    @JsonIgnore
+    private String password;
+
+    private String email;
+
+    @OneToMany(mappedBy = "account")
+    private Set<Note> notes = new HashSet<>();
+
+    Account() {
+    }
+
+    public Account(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Note> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(Set<Note> notes) {
+        this.notes = notes;
+    }
+
+    public int getNoteCount(){
+        return notes.size();
+    }
+}
